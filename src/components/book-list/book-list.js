37eeba449aa768f98af252a0dca40f436ba8cebc
dzmitriy-melnikov <dimaira2012@yandex.ru>
2019-04-24 23:51:22 +0300

    async data into store

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,11 +1,66 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { compose } from '../../utils';
+
+//import { bindActionCreators } from 'redux';
+
+import { booksLoaded } from '../../actions';
+
+import { withBookstoreService } from '../hoc'
+
+import BookListItem from '../book-list-item';
 
 import './book-list.css';
 
 class BookList extends Component {
-		render() {
-		
-		}
+
+	componentDidMount() {
+		const {bookstoreService, booksLoaded} = this.props;
+		const books = bookstoreService.getBooks();
+		console.log(books);
+		booksLoaded(books);
+	}
+
+	render() {
+		const { books } = this.props;
+		return(
+			<ul className="book-list">
+				{
+					books.map((book) => <li key={book.id}>
+					<BookListItem book={book}/>
+				</li>)
+				}
+			</ul>
+		)
+	}
 }
 
-export default BookList
+const mapStateToProps = ({books}) => {
+	return{
+		books
+	}
+};
+
+/*const mapDispatchToProps = (dispatch) => {
+	return {
+		booksLoaded: (newBooks) => dispatch(booksLoaded(newBooks))
+	}
+};*/
+
+/*const mapDispatchToProps = (dispatch) => bindActionCreators(
+	{
+		booksLoaded
+	}, dispatch);*/
+
+const mapDispatchToProps = {
+	booksLoaded
+};
+
+/*
+export default withBookstoreService()(connect(mapStateToProps, mapDispatchToProps)(BookList))*/
+
+export default compose(
+	withBookstoreService(),
+	connect(mapStateToProps, mapDispatchToProps)
+)(BookList)
--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import BookList from '../book-list';
+import ShoppingCartTable from '../shopping-cart-table/shopping-cart-table';
 
 const HomePage = () => {
-	return(
-		<div>
-			This is homepage
-		</div>
-	)
+  return (
+    <div>
+      <BookList />
+      <ShoppingCartTable />
+    </div>
+  );
 };
 
-export default HomePage
+export default HomePage;
